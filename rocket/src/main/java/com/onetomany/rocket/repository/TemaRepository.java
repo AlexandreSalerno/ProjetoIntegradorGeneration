841b05d23379bc90cbaf6451eb6d98391d408826
GiovannaTorres <giovannacltorres@gmail.com>
2021-05-31 11:18:56 -0300

    Tema Repository realizado

---
+++ rocket/src/main/java/com/onetomany/rocket/repository/TemaRepository.java
@@ -0,0 +1,16 @@
+package com.onetomany.rocket.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.onetomany.rocket.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository <Tema, Long>{
+	
+	public List <Tema> findAllByNomeContainingIgnoreCase(String nome);
+	
+
+}
