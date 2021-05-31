618572846febd04a1d85f8a69679a1b0ec3958b4
Guilherme-mendesc <guimendes1337@gmail.com>
2021-05-31 11:08:32 -0300

    Model Tema Realizado

---
+++ rocket/src/main/java/com/onetomany/rocket/model/Tema.java
@@ -0,0 +1,53 @@
+package com.onetomany.rocket.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 50)
+	private String nome;
+	
+	@NotNull
+	private int serie;
+	
+	
+	//Getters ands Setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getSerie() {
+		return serie;
+	}
+
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+	
+	
+}
