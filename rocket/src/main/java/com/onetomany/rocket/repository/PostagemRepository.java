6e0b336982457e188edca0b7f633242912e5a756
GiovannaTorres <giovannacltorres@gmail.com>
2021-06-04 11:16:25 -0300

    Postagem Repository e Controller

---
+++ rocket/src/main/java/com/onetomany/rocket/repository/PostagemRepository.java
@@ -0,0 +1,21 @@
+package com.onetomany.rocket.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.onetomany.rocket.model.Postagem;
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository <Postagem, Long>{
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+	public List<Postagem> findAllByConteudoContainingIgnoreCase(String conteudo);
+	
+	public List<Postagem> findAllByDate(Date date);
+
+}
